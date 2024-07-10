c650a99eab5f64489991786df8e8278ca0266b7a
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2024-07-10 18:07:33 +0800

    attempt 2

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <main className='py-10 bg-red-50 h-screen'> 
+      <h1 className='font-bold text-3xl text-center'>Your Todos</h1>
+    </main>
+  )
+}
+
+export default App
