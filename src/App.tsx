0e7de5d19aed51edebb4abc43b2806b99e8d5fbb
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2024-07-11 11:15:24 +0800

    temp 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { dummyData } from './data/todos'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -6,6 +7,15 @@ function App() {
   return (
     <main className='py-10 bg-red-50 h-screen'> 
       <h1 className='font-bold text-3xl text-center'>Your Todos</h1>
+      <div className='max-w-lg mx-auto'>
+        <div className='space-y-2'>
+          {dummyData.map(todo => (
+            <p key={todo.id} className='text-lg'>
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
--- src/data/todos.ts
+++ src/data/todos.ts
@@ -1,7 +1,19 @@
-export const dummyData = [
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
     {
         id: 1,
-        title: '',
+        title: 'Need to update into JIRA',
+        completed: false,
+    },
+    {
+        id: 2,
+        title: 'Eat on lunch',
+        completed: false,
+    },
+    {
+        id: 3,
+        title: 'Need to update into JIRA',
         completed: true,
-    }
+    },
 ]
